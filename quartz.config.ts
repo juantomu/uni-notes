c1a6502202f1f4e49e1b6dc1aa12d4d5d33f0bdc
Juan Pablo <juantomu1@gmail.com>
2024-10-06 18:11:09 -0500

    Quartz sync: Oct 6, 2024, 6:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.HardLineBreaks(),
+      Plugin.HardLineBreaks(), // Los enters para que funcionen como deberían
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
